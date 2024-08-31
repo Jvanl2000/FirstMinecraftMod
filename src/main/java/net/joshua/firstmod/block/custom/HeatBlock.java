a0356282ef057db4f3b979615aca703d36b10e0d
Jvanl2000 <jvanl65@tscstudents.net>
2024-08-31 19:30:19 +0000

    Heat block will now only damage you if you are not wearing any boots

--- src/main/java/net/joshua/firstmod/block/custom/HeatBlock.java
+++ src/main/java/net/joshua/firstmod/block/custom/HeatBlock.java
@@ -3,8 +3,11 @@ package net.joshua.firstmod.block.custom;
 import net.joshua.firstmod.item.ModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,12 +20,11 @@ public class HeatBlock extends Block {
 
     @Override
     public void stepOn(@NotNull Level pLevel, @NotNull BlockPos pPos,
-                       @NotNull BlockState pState, Entity pEntity) {
-        if (pEntity instanceof LivingEntity) {
-            if (!((LivingEntity) pEntity).isHolding(new ItemStack(ModItems.ALEXANDRITE.get()).getItem())) {
+                       @NotNull BlockState pState, @NotNull Entity pEntity) {
+        if (pEntity instanceof Player player) {
+            if (player.getItemBySlot(EquipmentSlot.FEET).equals(ItemStack.EMPTY)) {
                 pEntity.hurt(pLevel.damageSources().hotFloor(), 2.0F);
             }
         }
         }
-
 }
